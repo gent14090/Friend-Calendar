b9d1efe0773201c4fbff0226d40cb725ae5110d4
gent14090 <gent14090@gmail.com>
2023-05-31 11:03:33 +0200

    display a list of all events

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { useEffect, useState } from "react";
 
-export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
-};
+function EventsPage() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/events");
+      const data = await response.json();
+      setEvents(data);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Events</h1>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+          <p>{event.description}</p>
+          <img src={event.image} alt={event.title} />
+          <p>Start Time: {event.startTime}</p>
+          <p>End Time: {event.endTime}</p>
+          <p>Categories: {event.categories.join(", ")}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default EventsPage;
